fb05f9e99bdc661aeb2023978c14dbabf7b15042
AntaresMugisho <antaresmugisho@gmail.com>
2024-07-24 17:55:47 +0200

    Trying to get data from tables

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
+from pprint import pprint
+
 import requests
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup()
 
+def scrap_brochures(html: str):
+    soup = BeautifulSoup(html, "html.parser")
 
-def scrap(html):
-    print("Scrapping started...")
+    all_tables = soup.find_all("table")
 
-    # Parole parlée, brochures, livres, ...
+    author_tables = []
+    brochure_tables = []
+    for table in all_tables:
+        # Tables containing author's name has just 1 tr inside
+        if len(table.find_all("tr")) == 1:
+            author_tables.append(table)
+        else:
+            brochure_tables.append(table)
 
-    # author = table > tr[0] > td > font.text if len(tr) == 1
+    # Remove the last table as it's not an author table but a table used for copyright
+    author_tables.pop()
 
-    # category = tab > tr[1] > td > font.text
-    # tds [] = tab > tr[4] or tr[5] if category == "Traités"
+    #Remove the last table as it not contains books
+    brochure_tables.pop()
 
-    # title =  tds[2] > font.text
-    # html_content = tds[3] > div > font > a.href
-    # pdf_file = tds[4] > div > font > a if a.text == "Imprimer"
+    pprint(len(brochure_tables))
 
-    # Autres ...
+    data = {
+        "authors": [
+            {"name": "William Branham"},
+            {"name": "Ewald Franck"},
+            {"name": "Autres auteurs"},
+        ],
+    }
 
-    # Lettres circulaires => "http://cmpp.ch//lettre_circulaire.htm"
+    for table in all_tables:
+        if table in author_tables:
+            author = author_tables[0]
+
+        # category = tab > tr[1] > td > font.text
+        # tds [] = tab > tr[4] or tr[5] if category == "Traités"
+
+        # title =  tds[2] > font.text
+        # html_content = tds[3] > div > font > a.href
+        # pdf_file = tds[4] > div > font > a if a.text == "Imprimer"
+
+        # Autres ...
+
+        # Lettres circulaires => "http://cmpp.ch//lettre_circulaire.htm"
         # - Date, Contenu, Lien htm, Lien PDF
-    # Somaire
+        # Somaire
 
 
 if __name__ == "__main__":
     with open("brochures.html", "r") as file:
         html = file.read()
-        scrap(html)
+        scrap_brochures(html)
 
     # url = "http://cmpp.ch//brochures.htm"
     # url2 = "http://cmpp.ch//lettre_circulaire.htm"
